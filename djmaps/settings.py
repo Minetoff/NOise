d809dd70cec98cb32a2fc8e078fca9185791d2e1
Minetoff <lovygin2016@gmail.com>
2022-01-28 11:11:23 +0300

    templates in first page

--- djmaps/settings.py
+++ djmaps/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'djmaps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['maps/templates'],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
